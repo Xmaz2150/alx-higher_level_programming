ccbc2e1ef056ce243bba051e3299cdc4e4be5dfc
root <root@DESKTOP-K7H8GF6>
2023-12-19 13:59:50 +0200

    print n elements in list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed = 0
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            printed += 1
+        print()
+        return printed
+    except IndexError:
+        print()
+        return printed
