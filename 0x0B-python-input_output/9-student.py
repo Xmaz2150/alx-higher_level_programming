25e805f2bf1bfe248684ef9ec905f41b52aa8663
root <root@DESKTOP-K7H8GF6>
2024-01-09 21:14:35 +0200

    self to dict method

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" defines Student class """
+class_to_json = __import__('8-class_to_json').class_to_json
+
+
+class Student:
+    def __init__(self, first_name="", last_name="", age=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return class_to_json(self)
