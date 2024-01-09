1719ff2782a7d380fb3afce3a3e474ad067c0392
root <root@DESKTOP-K7H8GF6>
2024-01-09 22:32:11 +0200

    return specific attrs

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" defines Student class """
+
+
+class Student:
+    """ student class """
+
+    def __init__(self, first_name, last_name, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns clean attributes of
+            self """
+
+        if attrs is None:
+            return self.__dict__
+
+        og_dict = self.__dict__
+        new_dict_att = {}
+        for key in attrs:
+            new_val = og_dict.get(key)
+            if new_val is not None:
+                new_dict_att[key] = new_val
+        return new_dict_att
