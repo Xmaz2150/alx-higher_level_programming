06b86aa4881810ce8a605b409ab2704845286176
root <root@DESKTOP-K7H8GF6>
2024-01-09 21:17:12 +0200

    documentation

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,10 +4,13 @@ class_to_json = __import__('8-class_to_json').class_to_json
 
 
 class Student:
+    """ student class """
+
     def __init__(self, first_name="", last_name="", age=0):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ returns dict representation of self """
         return class_to_json(self)
