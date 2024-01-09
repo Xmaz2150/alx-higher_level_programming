e46b2a74f66689ed65d06611c2011ce6cec0a57a
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2024-01-09 21:37:39 +0200

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ defines Student class """
-class_to_json = __import__('8-class_to_json').class_to_json
 
 
 class Student:
     """ student class """
 
     def __init__(self, first_name, last_name, age):
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """ returns dict representation of self """
-        return class_to_json(self)
+
+        return self.__dict__
