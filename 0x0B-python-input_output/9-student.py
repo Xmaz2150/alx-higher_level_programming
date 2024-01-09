d7c2dcee4c998a4b9b1af05b217bcfef2038a993
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2024-01-09 21:35:15 +0200

    init origanal

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,7 +6,7 @@ class_to_json = __import__('8-class_to_json').class_to_json
 class Student:
     """ student class """
 
-    def __init__(self, first_name="", last_name="", age=0):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
