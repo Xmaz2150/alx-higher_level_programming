37664fae324705322ead72c8506811e9ff63757e
taf <muzirat0@gmail.com>
2024-01-16 18:34:30 +0200

    base class 1
    object count

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,10 +6,18 @@ class Base:
     """ base class for all future classes """
 
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
-            id = id
+            self.id = id
         else:
-            self.__nb__objects += 1
-            id = __nb__objects
+            self.inc_obj_no(1)
+            self.id = self.get_obj_no()
+
+    @classmethod
+    def get_obj_no(cls):
+        return cls.__nb_objects
+
+    @classmethod
+    def inc_obj_no(cls, n):
+        cls.__nb_objects += n
