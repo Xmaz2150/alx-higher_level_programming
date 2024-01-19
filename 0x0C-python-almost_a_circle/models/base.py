e13144fa19ae0ce85d41c63298ac594cec868bf2
taf <muzirat0@gmail.com>
2024-01-19 21:09:18 +0200

    base documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ init instance """
         if id is not None:
             self.id = id
         else:
@@ -16,20 +17,24 @@ class Base:
 
     @classmethod
     def get_obj_no(cls):
+        """ instance no getter """
         return cls.__nb_objects
 
     @classmethod
     def inc_obj_no(cls, n):
+        """ instance no setter """
         cls.__nb_objects += n
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ to json str"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """ saves to file """
         filename = f"{cls.__name__}.json"
 
         data = []
@@ -43,7 +48,8 @@ class Base:
             file.write(json_data)
 
     @staticmethod
-    def from_json_string(json_string)
+    def from_json_string(json_string):
+        """ obj from json """
         if not json_string:
             return []
         return json.loads(json_string)
