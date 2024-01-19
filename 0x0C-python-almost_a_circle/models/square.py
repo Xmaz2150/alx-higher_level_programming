a68ada27d69d37804b9aa200ff87b34874c5f2ad
taf <muzirat0@gmail.com>
2024-01-19 12:24:42 +0200

    square from rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" module for the square class """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ square class """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        str_square = "[Square] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_square + str_id + str_xy + str_wh
