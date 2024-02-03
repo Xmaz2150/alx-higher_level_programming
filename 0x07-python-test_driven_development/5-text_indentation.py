915f7144b6a9f5204b4174a47886bc840b6b0aac
taf <muzirat0@gmail.com>
2024-02-03 13:59:25 +0200

    correct type error message

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,35 +3,28 @@
 
 
 def text_indentation(text):
-   """
-   - Inserts two new lines after each period (.), question mark (?), or colon (:).
-   - Skips leading and extra spaces after these punctuation marks.
-   - Preserves existing newline characters (\n).
-
-   Args:
-       text: The text to be formatted and printed.
-
-   Raises:
-       TypeError: If the input text is not a string.
-   """
-
-   if not isinstance(text, str):
-       raise TypeError("text must be a string, not {}".format(type(text)))
-
-   index = 0  # Keep track of the current position in the text
-
-   while index < len(text):
-       character = text[index]
-       print(character, end="")
-
-       if character in ".?:":  # Handle punctuation marks
-           print("\n\n", end="")  # Add two new lines
-           index += 1  # Move to the next character
-           while index < len(text) and text[index] == " ":  # Skip spaces
-               index += 1
-           continue  # Skip to the next iteration
-
-       elif character == "\n":  # Preserve existing newlines
-           print()
-
-       index += 1  # Move to the next character
+    """
+    - Inserts two new lines after each period (.),
+    question mark (?), or colon (:).
+    - Skips leading and extra spaces after these punctuation marks.
+    - Preserves existing newline chars (\n).
+    Args:
+        text: The text to be formatted and printed.
+    Raises:
+    TypeError: If the input text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        char = text[i]
+        print(char, end="")
+        if char in ".?:":
+            print("\n\n", end="")
+            i += 1  # Move to the next char
+            while i < len(text) and text[i] == " ":
+                i += 1
+                continue
+        elif char == "\n":
+            print()
+        i += 1
