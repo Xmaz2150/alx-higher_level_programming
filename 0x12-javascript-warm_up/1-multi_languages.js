f0a23f6067877e4643ff65f2ec957b66b782c0db
tAf <muzirat0@gmail.com>
2024-03-11 16:22:38 +0200

    1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function printLanguagesMessage () {
+  const cLan = 'C is fun';
+  const pyLan = 'Python is cool';
+  const jsLan = 'JavaScript is amazing';
+
+  console.log(`${cLan}\n${pyLan}\n${jsLan}`);
+}
+
+printLanguagesMessage();
