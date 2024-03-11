bc4fe30c071d4586deaf3a524476b60cea9136f2
tAf <muzirat0@gmail.com>
2024-03-11 20:37:54 +0200

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return (1);
+  }
+  return n * factorial(n - 1);
+}
+
+const num = argv[2];
+console.log(factorial(num));
