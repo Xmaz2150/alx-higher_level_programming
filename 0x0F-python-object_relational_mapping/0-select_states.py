0c1bc74d69ef689e8d193b42462c3ec1b497a7aa
tAf <muzirat0@gmail.com>
2024-03-17 12:54:15 +0200

    Update
    import guard
    documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
+
+"""
+    lists states in database
+"""
+
 import MySQLdb
 from sys import argv
 
-ac = len(argv)
+if __name__ == '__main__':
+    ac = len(argv)
 
-if ac < 4:
-    exit(1)
-else:
-    user = argv[1]
-    passwd = argv[2]
-    db_name = argv[3]
+    if ac < 4:
+        exit(1)
+    else:
+        user = argv[1]
+        passwd = argv[2]
+        db_name = argv[3]
 
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=user,
-            passwd=passwd,
-            db=db_name,
-            charset="utf8"
-    )
+        db = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=user,
+                passwd=passwd,
+                db=db_name,
+                charset="utf8"
+        )
 
-    c = db.cursor()
+        c = db.cursor()
 
-    query = "SELECT * FROM {}.states".format(db_name)
-    c.execute(query)
+        query = "SELECT * FROM {}.states".format(db_name)
+        c.execute(query)
 
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
+        rows = c.fetchall()
+        for row in rows:
+            print(row)
 
-    c.close()
-    db.close()
+        c.close()
+        db.close()
