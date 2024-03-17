229211d71b9d07743af53a1dc97aae6c607b21e7
tAf <muzirat0@gmail.com>
2024-03-17 16:48:48 +0200

    6. First state model
    a python file that contains the class definition
     of a State and an instance Base = declarative_base()
    
    State class:
    	inherits from Base Tips
    	links to the MySQL table states
    	class attribute id that represents a column
    	of an auto-generated, unique integer, can’t be
    	null and is a primary key
    
    	class attribute name that represents a
    	column of a string with maximum 128 characters
    	and can’t be null

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+    state model module
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class
+    """
+
+    __tablename__ = 'states'
+    id = Column(
+            Integer,
+            autoincrement=True,
+            unique=True,
+            nullable=False,
+            primary_key=True
+    )
+    name = Column(String(128), nullable=False)
