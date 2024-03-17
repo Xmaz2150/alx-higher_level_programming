ff5dc6269f4588df466a78b7b2e6fdfafc0c92ad
tAf <muzirat0@gmail.com>
2024-03-17 18:17:16 +0200

    12. Update a state
    changes the name of a State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+    lists states in database
+"""
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
+
+if __name__ == '__main__':
+    ac = len(argv)
+
+    if ac < 4:
+        exit(1)
+    else:
+        user = argv[1]
+        passwd = argv[2]
+        db_name = argv[3]
+
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost/{}'.format
+                (user, passwd, db_name),
+                pool_pre_ping=True
+        )
+
+        Base.metadata.create_all(engine)
+
+        session = Session(engine)
+        state = session.query(State).filter_by(id=2).first()
+        state.name = "New Mexico"
+        session.add(state)
+        session.commit()
+
+        session.close()
