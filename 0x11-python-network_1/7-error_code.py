7dcf956e6f3fe76e44a7d85d81c47392e399db89
tAf <muzirat0@gmail.com>
2024-04-01 16:39:45 +0200

    7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" sends request to URL and displays the body of the response """
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
